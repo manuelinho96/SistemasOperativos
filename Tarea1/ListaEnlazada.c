3aad6f6916031786f4e629925c632358b323232a
Manuel Rodriguez <manuelbonilla96@hotmail.com>
2018-02-06 09:42:53 -0400

    Corregida la implementación de la lista
    Falta añadir una función para eliminar un solo elemento.

--- Tarea1/ListaEnlazada.c
+++ Tarea1/ListaEnlazada.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 struct Producto
 {
@@ -21,71 +23,96 @@ struct ListaEnlazada
 void Inicialize(struct ListaEnlazada * Lista)
 {
     Lista->head = NULL;
-}
+};
+
 
 int add(struct ListaEnlazada * Lista, struct Producto * Dato1)
 {
-    struct NodoLista * nodo;
+
+    struct NodoLista *nodo;
+    struct NodoLista *actual = Lista->head;
+    struct NodoLista *anterior = NULL;
+    nodo = malloc(sizeof(struct NodoLista));
     nodo->Dato = Dato1;
-    struct NodoLista * head = Lista->head;
-    if(head == NULL){
+    if(Lista->head == NULL){
         Lista->head = nodo;
+        Lista->head->next = NULL;
         return 0;
     }
-    struct NodoLista * actual = head;
-    struct NodoLista * anterior;
-    while(actual->next != NULL || actual->next->Dato->Peso <= Dato1->Peso ){
+    while(actual != NULL && actual->Dato->Peso <= Dato1->Peso){
         anterior = actual;
         actual = actual->next;
     }
-    if(actual->next == NULL){
-        if ( actual->next = (nodo *) malloc(sizeof(nodo)) == NULL){
-            return -1;
+    if ( actual == NULL ){
+        if (anterior != NULL) anterior->next = nodo;
+        else{
+            Lista->head = nodo;
         }
-        actual->next->next = NULL;
-        actual->next = nodo;
+        nodo->next = NULL;
         return 0;
     }
     else{
+        if (anterior == NULL){
+            Lista->head = nodo;
+            nodo->next = actual;
+            return 0;
+        }
         anterior->next = nodo;
         nodo->next = actual;
         return 0;
     }
+};
+
+int removelist(struct ListaEnlazada * Lista){
+    struct NodoLista * actual = Lista->head;
+    struct NodoLista *borrado;
+    while(actual != NULL){
+        borrado = actual;
+        actual = actual -> next;
+        free(borrado);
+    }
+    Lista->head = NULL;
 }
 
-int remove(struct ListaEnlazada * Lista, struct NodoLista * Nodo){
-    struct NodoLista * head = Lista->head;
-    if(head == NULL){
-        return 0;
-    }
-    struct NodoLista * anterior;
-    struct NodoLista * actual = head;
-    while(actual->next != NULL || actual->next != Nodo){
-        anterior = actual;
+
+void imprimirlista(struct ListaEnlazada *lista){
+    struct NodoLista *actual = lista->head;
+    while(actual != NULL){
+        printf("%s - ", actual->Dato->Nombre);
         actual = actual->next;
     }
-    if(actual->next == NULL){
-        return 0;
-    }
-    else{
-        anterior->next = actual->next;
-        free(actual);
-        return 0;
-    }
+    printf("\n");
 }
-int eliminar(struct ListaEnlazada * Lista){
-    struct NodoLista * head = Lista->head;
-    struct NodoLista * actual = Lista->head;
-    struct NodoLista * anterior;
-    if(head == NULL){
-        return 0;
-    }
-    while(actual->next != NULL)
-    {
-        anterior = actual;
-        remove(Lista, anterior);
-        actual = actual->next;
-    }
-    remove(Lista, actual);
-    return 0;
+
+
+int main(){
+
+    struct ListaEnlazada *l;
+    l = malloc(sizeof(struct ListaEnlazada));
+    Inicialize(l);
+    struct Producto *p;
+    p = malloc(sizeof(struct Producto));
+    strcpy(p->Nombre, "100");
+    p->Peso = 100;
+    add(l, p);
+    p = malloc(sizeof(struct Producto));
+    strcpy(p->Nombre, "50");
+    p->Peso = 50;
+    add(l, p);
+    p = malloc(sizeof(struct Producto));
+    strcpy(p->Nombre, "30");
+    p->Peso = 30;
+    add(l,p);
+    p = malloc(sizeof(struct Producto));
+    strcpy(p->Nombre, "75");
+    p->Peso = 70;
+    imprimirlista(l);
+    add(l, p);
+    removelist(l);
+    imprimirlista(l);
+    add(l, p);
+    imprimirlista(l);
+
 }
+
+

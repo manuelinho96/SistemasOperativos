2db017295cb5923a33b5f9e134793f4ce8234d87
manuelinho96 <manuelbonilla96@hotmail.com>
2018-03-21 22:24:36 -0400

    Creada la implementacion de hilos. Falta la implementacion de los hilos recursivos.

--- Tarea3/main.c
+++ Tarea3/main.c
@@ -2,25 +2,24 @@
 #include <string.h>
 #include <stdlib.h>
 #include <libgen.h>
+#include <pthread.h>   // Incluimos libreria necesaria para los threads
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <math.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sched.h>
+#include <dirent.h>
 
-/* // Nodo de claves asociadas a una ruta.
-typedef struct nodoclave{
-    char *clave;
-    struct clavescelda *next;
-}nodocelda;
 
-// Lista de claves
-typedef struct ListaClaves
-{
-    nodocelda *head;
-}ListaClaves;
-
-// Inicializar lista de claves
-void inicializarlistaclave(ListaClaves * Lista)
-{
-    Lista->head = NULL;
-}; */
-
+// Variable globales
+int maxlength;
 
 // Estructura de las celdas de la tabla
 typedef struct celda
@@ -28,6 +27,12 @@ typedef struct celda
     char *ruta;
     struct celda *next;
 } tcelda, *pcelda;
+
+// Pametros de indexacion
+typedef struct ParametrosIndexar{
+    char routename[PATH_MAX];
+    int height;
+}parametros;
 
 // Tabla de 100 slots
 #define B 100
@@ -43,9 +48,8 @@ void maketablenull(void){
 
 // Funcion de hash. Propuesta por Brian Kernighan y Dennis Ritchie
 unsigned int stringhash(char *string){
-    int i;
     unsigned int h = 0;
-    for(i = 0; *string; string++) h = 131*h + *string;
+    for(int i = 0; *string; string++) h = 131*h + *string;
     return(h%B);
 }
 
@@ -85,37 +89,46 @@ pcelda insertar(char *ruta, char*termbusq)
     else return (NULL);
 }
 
-// Buscador de la tabla de hash
-void buscarruta(char *termbusq){
+// Buscador de la tabla de 
+// Dado un termino de busqueda lo busca en la tabla de hash
+// Al entrar en la celda luego verifica que rutas cumplen con el termino
+// de busqueda indicado. Por ahora se asume que el nombre de los archivos
+// son con - para hacer el split.
+int buscarruta(char *termbusq){
     pcelda cp; /* current pointer */
     char *filename;
     char *claves;
+    int foundroute = 0;
 
     for(cp = hashtable[stringhash(termbusq)]; cp != NULL; cp = cp->next){
-        filename = strsave(basename(cp->ruta));
+        filename = strsave(basename(cp->ruta)); //basename devuelve el nombre de un archivo dado una ruta
         claves = strtok(filename,"-.");
         while( claves != NULL ){
-            if(strcmp(claves, termbusq) == NULL) printf("%s\n", cp->ruta); // Encontre un tembusq.
+            if(strcmp(claves, termbusq) == NULL){
+                printf("%s\n", cp->ruta);
+                foundroute++;
+            } // Encontre un tembusq.
             claves = strtok(NULL, "-.");
         }
     }
+    return foundroute;
 }
 
-int main(){
+// Funcion para cargar el indexador
+void *cargartable(void *args){
     FILE *indice;
-    indice = fopen("indice.txt", "r");
-    char caracteres[100];
+    char *nombre = (char *) args;
+    indice = fopen(nombre, "r");
     char *string;
     char *ruta;
     char *termbusq;
-    maketablenull();
-
+    char caracteres[NAME_MAX];
     if (indice == NULL){
         exit(1);
     }
     else{
         while ( !feof(indice) ){
-            fgets(caracteres,1000,indice);
+            fgets(caracteres,NAME_MAX,indice);
             string = strtok (caracteres," ");
             ruta = strsave(string);
             while (string != NULL){
@@ -126,6 +139,109 @@ int main(){
             insertar(ruta, termbusq);
         }
     }
-    buscarruta("manuel");
     fclose(indice);
+    return NULL;
+}
+
+void navegar_directorio(char* routename, int heightvalue){
+
+    DIR *dir;
+    struct dirent *entry;
+    char path[PATH_MAX];
+    char *archivoaux;
+    char *claves;
+    int estado;
+    if( maxlength != heightvalue ){
+        if (!(dir = opendir(routename))){
+            return NULL;
+        }
+        while((entry = readdir(dir)) != NULL){
+            if (entry->d_type == DT_DIR) {
+                if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) continue;
+                    heightvalue++;
+                    snprintf(path, sizeof(path), "%s/%s/", routename, entry->d_name);
+                    navegar_directorio(path, heightvalue);
+            } 
+                else {
+                    snprintf(path, sizeof(path), "%s%s", routename, entry->d_name);
+                    archivoaux = strsave(entry->d_name);
+                    claves = strtok(archivoaux, "-.");
+                    while( claves != NULL ){
+                        estado = buscarruta(claves);
+                        if (estado == 0){
+                            printf("insertare el path %s con la clave %s\n", path, claves);
+/*                             insertar(path, claves); */
+                        }
+                        claves = strtok(NULL, "-.");
+                    }
+                }
+        }
+    }
+    return NULL;
+
+}
+
+void *indizar(void *args){
+
+    
+    DIR *dir;
+    struct dirent *entry;
+    char path[PATH_MAX];
+    char *archivoaux;
+    char *claves;
+    parametros *p = (parametros *) args;
+    char *routename = (char *) p->routename;
+    int heightvalue = (int) p->height;
+    int estado;
+    if( maxlength != heightvalue ){
+        if (!(dir = opendir(routename))){
+            return NULL;
+        }
+        while((entry = readdir(dir)) != NULL){
+            if (entry->d_type == DT_DIR) {
+                if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) continue;
+                    heightvalue++;
+                    snprintf(path, sizeof(path), "%s/%s/", routename, entry->d_name);
+                    navegar_directorio(routename, heightvalue);
+            } 
+                else {
+                    snprintf(path, sizeof(path), "%s/%s", routename, entry->d_name);
+                    archivoaux = strsave(entry->d_name);
+                    claves = strtok(archivoaux, "-.");
+                    while( claves != NULL ){
+                        estado = buscarruta(claves);
+                        if (estado == 0){
+                            printf("insertare el path %s con la clave %s\n", path, claves);
+/*                             insertar(path, claves); */
+                        }
+                        claves = strtok(NULL, "-.");
+                    }
+                }
+        }
+    }
+    return NULL;
+}
+
+int main(){
+
+    maxlength = 20;
+    char nombre[200] = "indice.txt";
+    maketablenull();
+    pthread_t cargarindice;
+    pthread_t buscarindice;
+    pthread_t indezador;
+    if (pthread_create ( &cargarindice, NULL, &cargartable, nombre) != 0){
+        printf("Error al crear el hilo. \n"); 
+        exit(EXIT_FAILURE); 
+    }
+    pthread_join(cargarindice, NULL);
+    parametros *p = malloc(sizeof(parametros));
+    p->height = 0;
+    strcpy(p->routename, ".");
+    if (pthread_create ( &indezador, NULL, &indizar, p) != 0){
+        printf("Error al crear el hilo. \n"); 
+        exit(EXIT_FAILURE); 
+    }
+    pthread_join(indezador, NULL);
+    buscarruta("manuel");
 }
